356e91aa9568ea9e7a3886704e836c73ca7c5959
suleimanzainab <157100053+suleimanzainab@users.noreply.github.com>
2024-05-22 13:19:23 +0200

    Update settings.py

--- Photography/settings.py
+++ Photography/settings.py
@@ -166,7 +166,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL ='static/'
 MEDIA_URL = '/media/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 AUTH_USER_MODEL='accounts.User'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
